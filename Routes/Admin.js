e9b39dbeed8a0cbe1b50b307f6c260055aecd77b
Alzahraa Desoky <33639931+alzahra151@users.noreply.github.com>
2023-08-20 19:54:54 +0300

    update

--- Routes/Admin.js
+++ Routes/Admin.js
@@ -18,7 +18,7 @@ router.post('/Login', async (req, res, next) => {
         const StoredAdmin = await Login(req.body)
         const token = StoredAdmin.result.AccessToken
         // res.cookie('token', token, { maxAge: 3600000, httpOnly: true, secure: true })
-        res.status(200).json({ success: true, message: 'Login successful', token: token });
+        res.status(200).json(StoredAdmin);
     } catch (err) {
         res.status(401).json(err.message)
     }

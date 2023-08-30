40abf9cec59dc288c9b3835b81f207feb736b3b9
Alzahraa Desoky <33639931+alzahra151@users.noreply.github.com>
2023-08-30 20:04:22 +0300

    update pdf

--- Routes/GeneratePDF.js
+++ Routes/GeneratePDF.js
@@ -79,13 +79,13 @@ router.post('/Download', async (req, res, next) => {
                     right: "0px"
                 }
             };
-            pdf.create(data, options).toFile("../PDF/VarroOffer.pdf", function (err, data) {
+            pdf.create(data, options).toFile("./PDF/VarroOffer.pdf", function (err, data) {
                 if (err) {
                     res.send(err);
                     console.log(err)
                 } else {
                     const filePath = '../PDF/VarroOffer.pdf';
-                    const fileName = 'varroxOffer.pdf';
+                    const fileName = 'VarroOffer.pdf';
                     fs.readFile(filePath, function (err, data) {
                         if (err) {
                             console.error(err);

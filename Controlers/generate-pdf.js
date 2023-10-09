494a4d3192baff488931f29b8b4fd6d9a95572a4
Alzahra mohamed <alzahradesoky.15@gmail.com>
2023-10-09 13:19:21 +0300

    add docker file

--- Controlers/generate-pdf.js
+++ Controlers/generate-pdf.js
@@ -1,35 +1,35 @@
 const ejs = require("ejs");
-const chrome = require("chrome-aws-lambda");
 const puppeteer = require('puppeteer')
-// if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-//     chrome = require("chrome-aws-lambda");
-//     puppeteer = require("puppeteer-core");
-// } else {
-//     puppeteer = require("puppeteer");
-// }
+require("dotenv").config()
+
 async function generatePdf(data) {
-    console.log("test")
-    console.log(data)
+
     let browser;
     try {
         browser = await puppeteer.launch({
-            args: ["--hide-scrollbars", "--disable-web-security"],
-            // defaultViewport: chrome.defaultViewport,
-            executablePath: "./.cache/puppeteer/chrome/win64-1108766/chrome-win/chrome.exe",
-            headless: true,
-            ignoreHTTPSErrors: true,
+            args: [
+                "--disable-setuid-sandbox",
+                "--no-sandbox",
+                "--single-process",
+                "--no-zygote",
+            ],
+            executablePath:
+                process.env.NODE_ENV === "production"
+                    ? process.env.PUPPETEER_EXECUTABLE_PATH
+                    : puppeteer.executablePath(),
         });
         const [page] = await browser.pages();
+
         const html = await ejs.renderFile("./views/OfferMail.ejs", { data: data })
-        // console.log(html)
         await page.setContent(html, { waitUntil: 'networkidle0' });
         await page.emulateMediaType('screen');
         const pdf = await page.pdf({
-            // path: `${data.QrCode}.pdf`,
+
             format: 'A4',
             printBackground: true
         });
-        // console.log(pdf.path)
+
+        browser.close();
         return pdf
 
     } catch (err) {

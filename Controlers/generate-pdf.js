f414dedd75d803ed8f5b8129f98bd649d98a5d27
Alzahraa Desoky <33639931+alzahra151@users.noreply.github.com>
2023-10-09 13:22:25 +0300

    Update generate-pdf.js

--- Controlers/generate-pdf.js
+++ Controlers/generate-pdf.js
@@ -1,31 +1,21 @@
 const ejs = require("ejs");
-const chrome = require("chrome-aws-lambda");
-const { PUPPETEER_REVISIONS } = require('puppeteer-core');
-const puppeteer = require('puppeteer-core')
-const { install } = require('@puppeteer/browsers');
-const config = require('../puppeteer.config.cjs');
-const buildId = PUPPETEER_REVISIONS?.chrome;
-const cacheDir = config.cacheDirectory;
+const puppeteer = require('puppeteer')
+
 async function generatePdf(data) {
-    // console.log("test")
-    // console.log(data)
-    let platform = 'windows'
-    console.log('Installing Chrome version', buildId, 'for platform', platform, 'to', cacheDir);
-    try {
-        await install({ platform, browser: 'chrome', buildId, cacheDir });
-        console.log('Chrome installed successfully');
-    } catch (err) {
-        console.error('Chrome installation failed', err);
-        throw err;
-    }
+  
     let browser;
     try {
         browser = await puppeteer.launch({
-            args: ["--hide-scrollbars", "--disable-web-security"],
-            // defaultViewport: chrome.defaultViewport,
-            executablePath: "./.cache/puppeteer/chrome/win64-1108766/chrome-win/chrome.exe",
-            headless: false,
-            ignoreHTTPSErrors: true,
+            args: [
+                "--disable-setuid-sandbox",
+                "--no-sandbox",
+                "--single-process",
+                "--no-zygote",
+            ],
+            executablePath:
+                process.env.NODE_ENV === "production"
+                    ? process.env.PUPPETEER_EXECUTABLE_PATH
+                    : puppeteer.executablePath(),
         });
         const [page] = await browser.pages();
         const html = await ejs.renderFile("./views/OfferMail.ejs", { data: data })

37db32a63116683e70628f4e9e0304a1f35f539b
Alzahra mohamed <alzahradesoky.15@gmail.com>
2023-10-09 10:11:22 +0300

    update puppeter aws lambada

--- Controlers/generate-pdf.js
+++ Controlers/generate-pdf.js
@@ -1,13 +1,24 @@
 const ejs = require("ejs");
-
-const puppeteer = require('puppeteer')
-
+const chrome = require("chrome-aws-lambda");
+const puppeteer = require('puppeteer-core')
+// if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+//     chrome = require("chrome-aws-lambda");
+//     puppeteer = require("puppeteer-core");
+// } else {
+//     puppeteer = require("puppeteer");
+// }
 async function generatePdf(data) {
     console.log("test")
     console.log(data)
     let browser;
     try {
-        browser = await puppeteer.launch({ headless: false, args: ['--disable-extensions', '--disable-web-security'], });
+        browser = await puppeteer.launch({
+            args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
+            defaultViewport: chrome.defaultViewport,
+            executablePath: await chrome.executablePath,
+            headless: true,
+            ignoreHTTPSErrors: true,
+        });
         const [page] = await browser.pages();
         const html = await ejs.renderFile("./views/OfferMail.ejs", { data: data })
         // console.log(html)

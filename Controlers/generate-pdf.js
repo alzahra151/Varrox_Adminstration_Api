cbd22def3fb032695bbee5d21671502d0eb2daf5
Alzahraa Desoky <33639931+alzahra151@users.noreply.github.com>
2023-10-10 10:36:10 +0300

    Update generate-pdf.js

--- Controlers/generate-pdf.js
+++ Controlers/generate-pdf.js
@@ -7,7 +7,7 @@ async function generatePdf(data) {
     let browser;
     try {
         browser = await puppeteer.launch({
-            headless: false,
+            headless: true,
             args: [
                 "--disable-setuid-sandbox",
                 "--no-sandbox",

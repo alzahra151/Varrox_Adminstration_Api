385ddf27d6f4863780e7da74062310f193ac8b70
Alzahra mohamed <alzahradesoky.15@gmail.com>
2023-10-14 17:07:28 +0300

    add final updates

--- Routes/GeneratePDF.js
+++ Routes/GeneratePDF.js
@@ -4,7 +4,9 @@ const router = express.Router();
 const { SendEmail } = require("../Controlers/NodeMailer");
 const QRCode = require('qrcode');
 const { generatePdf } = require("../Controlers/generate-pdf")
-
+const pdf = require('html-pdf')
+const ejs = require('ejs')
+const fs = require('fs')
 router.post("/SendEmail", async (req, res, next) => {
   let OfferData = req.body;
   const qrCode = OfferData.QrCode
@@ -45,6 +47,7 @@ router.post('/down-pdf', async (req, res) => {
         "Content-Disposition",
         `attachment; filename=${OfferData.QrCode}.pdf`
       );
+      res.setHeader("Access-Control-Allow-Headers", "X-Requested-With, content-type");
       console.log(pdf)
       res.send(pdf)
       // res.status(200).json(newBlob)
@@ -54,5 +57,90 @@ router.post('/down-pdf', async (req, res) => {
   })
 })
 
+router.post("/Download", async (req, res, next) => {
+  let OfferData = req.body;
+  // let code = 129
+  // const qrcode = code++
+  // const stringQrCode = qrcode.toString()
+  // console.log(stringQrCode)
+  // const qr = await QRLogo.generateQRWithLogo(stringQrCode, "logo.png", {}, "PNG", "public/images/Varrox-Logo.png")
+  // console.log(qr)
+  // QRCode.toDataURL(stringQrCode, async (err, qrCodeDataURL) => {
+  //   if (err) {
+  //     console.error(err);
+  //     return;
+  //   }
+  //   OfferData.qrCodeImage = qrCodeDataURL
+  console.log(OfferData)
+  await ejs.renderFile("./views/OfferMail.ejs", { data: OfferData },
+    (err, data) => {
+      //create pdf
+      if (err) {
+        res.send(err);
+        console.log("faild render", data);
+      } else {
+        var options = {
+          // format: 'A4',
+          // border: '10mm',
+          height: "10.5in",
+          width: "7in",
+          border: {
+            top: "1px",
+            bottom: "1px",
+            left: "1px",
+            right: "1px",
+
+          },
+          margin: {
+            top: "0px",
+            bottom: "0px",
+            left: "0px",
+            right: "0px",
+          },
+          padding: {
+            top: "5px",
+            bottom: "0px",
+            left: "5px",
+            right: "0px",
+          },
+          childProcessOptions: {
+            env: {
+              OPENSSL_CONF: "/dev/null",
+            },
+          },
+        };
+        console.log(options);
+        pdf
+          .create(data, options)
+          .toFile("./PDF/VarroOffer.pdf", function (err, data) {
+            if (err) {
+              console.log(data)
+              console.log(err.message);
+              res.send(err);
+            } else {
+              const filePath = "./PDF/VarroOffer.pdf";
+              const fileName = "VarroOffer.pdf";
+              fs.readFile(filePath, function (err, data) {
+                if (err) {
+                  console.error(err);
+                  res.status(500).send("Error reading file");
+                } else {
+                  res.setHeader("Content-Type", "application/pdf");
+                  res.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+                  res.setHeader("Access-Control-Allow-Headers", "X-Requested-With, content-type");
+                  res.setHeader(
+                    "Content-Disposition",
+                    "attachment; filename=" + fileName
+                  );
+                  res.send(data);
+                }
+              });
+            }
+          });
+      }
+    }
+  );
+});
+// });
 
 module.exports = router;
